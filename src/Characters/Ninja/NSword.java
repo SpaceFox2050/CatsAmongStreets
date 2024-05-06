b0e02844c7cee1a30ab7f3e1ef127763eeaa3336
Matthew Ji <93742983+mynteee@users.noreply.github.com>
2024-05-06 02:35:47 -0700

    sword fix

--- src/Characters/Ninja/NSword.java
+++ src/Characters/Ninja/NSword.java
@@ -5,8 +5,7 @@ public class NSword extends DamageEntity{
     
     public NSword(Main main, Vector2D position, int player){
         super(main, position, 60, 20, 2, player, 200);
-        //temporary sword pic, not that we need a new sword pic anyway
-        ninjasword = new Asset("assets/characters/sword_p1-01", new Vector2D(0,0), 4); 
+        ninjasword = new Asset("assets/characters/stab/sword.png", new Vector2D(0,0), 4); 
 
         ninjasword.setVisible(true);
         add(ninjasword);
