5e8021705c3271ec273f5671d6a5f96dc1e245ee
Matthew Ji <93742983+mynteee@users.noreply.github.com>
2024-05-06 02:47:36 -0700

    Made the robot work

---
+++ src/Robot/Robot.java
@@ -0,0 +1,136 @@
+import java.util.ArrayList;
+
+import ky.Asset;
+import ky.Vector2D;
+import ky.AudioPlayer;
+
+public class Robot extends Character {
+
+    double attackTime;
+    double enrageTime = 1;
+    Rocket[] rockets;
+    Laser laser;
+    int curRocket = 0;
+    AudioPlayer zapSound;
+    AudioPlayer rocketwhoosh;
+
+    public Robot(Main main) {
+        super(new Vector2D(0, 0), 75, 100, 1200, main);
+    }
+
+    public Robot(Vector2D position, Main main) {
+        super(position, 75, 100, 1200, main);
+    }
+
+    @Override
+    public void updateCharacter(double deltaT, ArrayList<Integer> keyCodes) {
+        if (status == Status.ATTACKING) {
+            attackTime -= deltaT;
+            if (attackTime <= 0) {
+                laser.canDamage = false;
+                laser.setVisible(false);
+                status = Status.IDLE;
+            }
+        }
+
+        if (characterAsset.getImageIndex() == 1) {
+            enrageTime -= deltaT;
+            if (enrageTime <= 0) {
+                enrageTime = 1;
+                characterAsset.setImageIndex(0);
+            }
+        }
+    }
+
+    @Override
+    public void initialize() {
+        setStats("characterStats/spaceship.stats");
+
+        /*characterAsset = new Asset(new String[] {
+                "assets/characters/robot/spaceship_normal.png",
+                "assets/characters/robot/spaceship_enraged.png"
+        }, new Vector2D(0, 0), 3);*/
+
+        if(getPlayer() == 1){
+            characterAsset = new Asset(new String[] {
+                "assets/characters/robot/robot_p1.png",
+                "assets/characters/robot/robot_mad_p1.png"
+            }, new Vector2D(0, 0), 64, 64, 3);
+        }else{
+            characterAsset = new Asset(new String[] {
+                "assets/characters/robot/robot_p2.png",
+                "assets/characters/robot/robot_mad_p2.png"
+            }, new Vector2D(0, 0), 64, 64, 3);
+        }
+
+        characterAsset.setVisible(true);
+        characterAsset.rescale(2);
+        add(characterAsset);
+
+        rockets = new Rocket[10];
+        for (int i = 0; i < 10; i++) {
+            if(getPlayer() == 1){
+                rockets[i] = new Rocket(main, "assets/characters/robot/rocket_p1.png", new Vector2D(0, 0), getPlayer(), 500);
+            }else{
+                rockets[i] = new Rocket(main, "assets/characters/robot/rocket_p2.png", new Vector2D(0, 0), getPlayer(), 500);
+            }
+            
+            add(rockets[i]);
+        }
+        laser = new Laser(main, new Vector2D(0, 0), getPlayer());
+        add(laser);
+
+        zapSound = new AudioPlayer("assets/SFX/zap.wav");
+        zapSound.setVolume(-30);
+
+        rocketwhoosh = new AudioPlayer("assets/SFX/rocket.wav");
+
+        abilityIcon = new Asset("assets/characters/robot/ability_icon.png", new Vector2D(0, 0), 4);
+        ultIcon = new Asset("assets/characters/robot/ult_icon.png", new Vector2D(0, 0), 4);
+    }
+
+    @Override
+    protected void basicAttack() {
+        if (status == Status.IDLE) {
+            status = Status.ATTACKING;
+            attackTime = 0.3;
+            laser.setActive();
+
+            zapSound.reset();
+            zapSound.play();
+        }
+    }
+
+    @Override
+    protected void basicAbility() {
+        rocketwhoosh.reset();
+        rocketwhoosh.play();
+        if (curRocket >= rockets.length) {
+            curRocket = 0;
+        }
+        rockets[curRocket].setDamage(500);
+        rockets[curRocket].setPos(getPos());
+        rockets[curRocket].homing = false;
+        rockets[curRocket].setActive();
+        curRocket++;
+    }
+
+    @Override
+    protected void ultimate() {
+        rocketwhoosh.reset();
+        rocketwhoosh.play();
+        characterAsset.setImageIndex(1);
+        for (int i = 0; i < 4; i++) {
+            if (curRocket >= rockets.length) {
+                curRocket = 0;
+            }
+            rockets[curRocket].setDamage(250);
+            rockets[curRocket].setPos(new Vector2D(getX(), getY() - 250 + i * 100));
+            rockets[curRocket].setVisible(true);
+            rockets[curRocket].canDamage = true;
+            rockets[curRocket].homing = true;
+            curRocket++;
+        }
+    }
+
+}
---
+++ src/Robot/Rocket.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+import ky.Asset;
+import ky.CollisionEntity;
+import ky.Vector2D;
+//import java.awt.image.BufferedImage;
+
+public class Rocket extends DamageEntity {
+
+    Character testChar;
+    double speed = 2000;
+    Asset rocketAsset;
+    boolean homing = true;
+    double angle = 0;
+
+
+    public Rocket(Main main, String image, Vector2D position, int player, double dmg) {
+        super(main, position, 32, 64, 3, player, dmg, 1500);
+        testChar = ((player == 1) ? main.player2 : main.player1);
+        setBreaks(true);
+        rocketAsset = new Asset(image, new Vector2D(0, 0), 64, 64, 3);
+        rocketAsset.setVisible(true);
+        add(rocketAsset);
+    }
+
+    public void setActive() {
+        angle = ((testChar.equals(main.player2)) ? main.player1.direction.getValue()
+                : main.player2.direction.getValue());
+        angle = ((angle == 1) ? 0 : -Math.PI);
+        setVisible(true);
+        canDamage = true;
+    }
+
+    @Override
+    public void updateEntity(double deltaT, ArrayList<Integer> keyCodes) {
+        if (testChar != null && this.isVisible()) {
+            if (homing) {
+                angle = Math.atan2(testChar.getY() - getY(),
+                        testChar.getX() - getX());
+
+            }
+            setVel(new Vector2D(Math.cos(angle) * speed, Math.sin(angle) * speed));
+            setRotation(angle * 180 / Math.PI + 90);
+
+        } else {
+            setVel(new Vector2D(0, 0));
+        }
+    }
+
+    @Override
+    public void onCollision(CollisionEntity collidingEntity) {
+        super.onCollision(collidingEntity);
+    }
+}
